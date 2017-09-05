b9f7856c28498bddc902dd4251409c1c418e63f5
Reiss Johnson <reissjohnson@me.com>
2017-09-05 13:32:18 +0100

    put conditional onto ticket for updating remote, created update_timesheet method

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,19 +1,22 @@
 class Ticket < ApplicationRecord 
   has_many :timesheets
-  after_commit :update_remote, on: :update
+  after_commit :update_remote, on: :update, if: :state_previously_changed?
 
-  private
+  def update_timesheet
+    timesheets.create!(
+      started_at: Time.now,
+      state: state,
+      before_state: state_previous_change.first
+    )
+  end
 
   def update_remote
     # TODO: need to be able to recover if github does not respond, 
     # possibly moving to a background job
-    if state_previously_changed?
-      if state == 'Closed'
-        Octokit.close_issue('createkio/flight_plan', remote_number)
-      elsif state_previous_change.first == 'Closed'
-        Octokit.reopen_issue('createkio/flight_plan', remote_number)
-      end
+    if state == 'Closed'
+      Octokit.close_issue('createkio/flight_plan', remote_number)
+    elsif state_previous_change.first == 'Closed'
+      Octokit.reopen_issue('createkio/flight_plan', remote_number)
     end
   end
-
 end
