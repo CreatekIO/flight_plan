8c8f3e95bb343f30bcd3d5682becc29ff268f708
John Cleary <john@createk.io>
2017-09-05 10:18:14 +0100

    update GitHub when an issue is closed

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,15 @@
 class Ticket < ApplicationRecord
+
+  after_commit :update_remote
+
+  private
+
+  def update_remote
+    # TODO: need to be able to recover if github does not respond, 
+    # possibly moving to a background job
+    if state == 'Closed'
+      Octokit.close_issue('createkio/flight_plan', remote_number)
+    end
+  end
+
 end
