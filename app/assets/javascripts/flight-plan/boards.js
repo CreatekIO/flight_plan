ab21a3d46692cb62b960656cd4a3f66b7265d963
Brandon Burton <brandon@createk.io>
2017-09-05 14:34:00 +0100

    extract show comments function, make modal generic

--- app/assets/javascripts/flight-plan/boards.js
+++ app/assets/javascripts/flight-plan/boards.js
@@ -1,26 +1,32 @@
 FPLAN.boards = {
   show: function() {
-    $('#ticketModal').on('show.bs.modal', function(event) {
-      var link = $(event.relatedTarget);
-
-      $.ajax({
-        url: link.data('url')
-      }).success(function(data) {
-        var modal = $('#ticketModal');
-        var modalBody = modal.find('.modal-body')
-
-        modal.find('.modal-title').html(data.title);
-        modalBody.html('<div class="well">' + data.body + '</div>');
-        $.each(data.comments, function(index, comment) {
-          modalBody.append(
-            '<div class="well">'
-              + comment.body
-              + '<br><span class="label label-danger">'
-              + comment.author
-              + '</span>'
-              + '</div>');
-        })
-      })
+    var parent = this;
+    $('#applicationModal').on('show.bs.modal', function(event) {
+      var relatedTarget = $(event.relatedTarget);
+      if(relatedTarget.data('hook').length > 0) {
+        eval('parent.' + relatedTarget.data('hook') + '(relatedTarget)')
+      }
     });
+  },
+
+  _showTicketAjax: function(link) {
+    $.ajax({
+      url: link.data('url')
+    }).success(function(data) {
+      var modal = $('#applicationModal');
+      var modalBody = modal.find('.modal-body')
+
+      modal.find('.modal-title').html(data.title);
+      modalBody.html('<div class="well">' + data.body + '</div>');
+      $.each(data.comments, function(index, comment) {
+        modalBody.append(
+          '<div class="well">'
+            + comment.body
+            + '<br><span class="label label-danger">'
+            + comment.author
+            + '</span>'
+            + '</div>');
+      })
+    })
   }
 }
