7437743c85b9d9415ecd106272fd46cabb03d54b
John Cleary <john@createk.io>
2017-09-05 10:28:46 +0100

    allow issues to be reopened

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -7,8 +7,12 @@ class Ticket < ApplicationRecord
   def update_remote
     # TODO: need to be able to recover if github does not respond, 
     # possibly moving to a background job
-    if state == 'Closed'
-      Octokit.close_issue('createkio/flight_plan', remote_number)
+    if state_previously_changed?
+      if state == 'Closed'
+        Octokit.close_issue('createkio/flight_plan', remote_number)
+      elsif state_previous_change.first == 'Closed'
+        Octokit.reopen_issue('createkio/flight_plan', remote_number)
+      end
     end
   end
 
