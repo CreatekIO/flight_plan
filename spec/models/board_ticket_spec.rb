1d32432363d3c710b8234cc10953aa21f08b6e20
John Cleary <john@createk.io>
2017-10-12 13:36:49 +0100

    move ticket tests into board ticket spec as thats where the logic now resides

--- spec/models/board_ticket_spec.rb
+++ spec/models/board_ticket_spec.rb
@@ -6,4 +6,69 @@ RSpec.describe BoardTicket, type: :model do
     it { is_expected.to belong_to(:ticket) }
     it { is_expected.to belong_to(:swimlane) }
   end
+
+  describe '#save' do
+    let(:repo) { create(:repo) }
+    let(:board) { create(:board, repos: [ repo ]) }
+    let!(:backlog) { create(:swimlane, name: 'Backlog', board: board) }
+    let!(:dev) { create(:swimlane, name: 'Dev', board: board) }
+    let!(:closed) { create(:swimlane, name: 'Closed', board: board) }
+
+    let(:ticket) { create(:ticket, remote_number: '4', repo: repo) }
+    subject { create(:board_ticket, board: board, ticket: ticket, swimlane: backlog) }
+
+    before(:each) do
+      subject # force creation of subject in database
+    end
+
+    context 'when an issue is moved to "Closed"' do
+      it 'updates the GitHub issue via the API' do
+        stub = stub_request(:patch, "https://api.github.com/repos/createkio/flight_plan/issues/4").
+          with(body: "{\"state\":\"closed\"}")
+        subject.swimlane = closed
+        subject.save
+
+        expect(stub).to have_been_requested
+      end
+    end
+
+    context 'when an issue is moved from "Closed"' do
+      subject { create(:board_ticket, swimlane: closed, ticket: ticket, board: board) }
+
+      it 'updates the GitHub issue via the API' do
+        stub = stub_request(:patch, "https://api.github.com/repos/createkio/flight_plan/issues/4").
+          with(body: "{\"state\":\"open\"}")
+        subject.swimlane = backlog
+        subject.save
+
+        expect(stub).to have_been_requested
+      end
+    end
+
+    context 'with no state change' do
+      it 'doesn\'t create/update any new timesheets' do
+        expect{ subject.touch }.to_not change{ Timesheet.count }
+      end
+    end
+
+    context 'when state changes' do
+      subject { create(:board_ticket, swimlane: backlog, ticket: ticket, board: board) }
+
+      it 'creates a new open timesheet' do
+        expect {
+          subject.update_attributes(swimlane_id: dev.id)
+        }.to change {
+          subject.timesheets.where(swimlane_id: dev.id, before_swimlane_id: backlog.id, ended_at: nil, after_swimlane_id: nil).count
+        }
+      end
+
+      it 'closes the open timesheet' do
+        expect {
+          subject.update_attributes(swimlane_id: dev.id)
+        }.to change {
+          subject.reload.open_timesheet.id
+        }
+      end
+    end
+  end
 end
