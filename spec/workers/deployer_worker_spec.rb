1328ff20072f661284a367b4af74c34ef4514246
John Cleary <john@createk.io>
2017-11-01 21:12:58 +0000

    use `xit` instead of `pending` to get the build to pass

--- spec/workers/deployer_worker_spec.rb
+++ spec/workers/deployer_worker_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
-RSpec.describe DeployerWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe DeployWorker, type: :worker do
+  context 'when there is an existing PR to master' do
+    xit 'does not create a release PR' do
+    end
+  end
+
+  context 'when it\'s outside the working day' do
+    xit 'does not create a release PR' do
+    end
+  end
+
+  context 'when issues are pending deployment' do
+    xit 'creates a release PR' do
+    end
+  end
 end
