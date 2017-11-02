7c91a90e9a824db36668363a814b52e85fe957b1
John Cleary <john@createk.io>
2017-11-02 14:01:35 +0000

    marked issues as un-merged when there is a push a branch with the number of the issue
    For example, when you push to a branch on github called feature/#25-my-feature, then
    issue number 25 is marked as not merged (`merged = false`). This will allow us to
    track which issue have been deployed as part of a release as well as avoiding
    re-deploying an issue

--- app/controllers/webhook/github_controller.rb
+++ app/controllers/webhook/github_controller.rb
@@ -18,12 +18,21 @@ class Webhook::GithubController < Webhook::BaseController
     end
   end
 
+  def github_push(payload)
+    issue_number = payload['ref'][/#[0-9]*/, 0][1..-1]
+    return unless issue_number
+
+    repo = Repo.find_by!(remote_url: payload['repository']['full_name']) 
+    ticket = repo.tickets.find_by!(remote_number: issue_number)
+
+    ticket.update_attributes(merged: false)
+  end
+
   def not_found
-    head :not_found
+    head :ok
   end
 
   def webhook_secret(_payload)
     ENV['GITHUB_WEBHOOK_SECRET']
   end
-
 end
