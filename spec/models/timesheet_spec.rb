d5a0587881e5978936564b184f04d87015562014
John Cleary <john@createk.io>
2017-11-03 13:45:03 +0000

    add pending tests

---
+++ spec/models/timesheet_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Timesheet do
+  describe 'associations' do
+    it { is_expected.to belong_to(:board_ticket) }
+    it { is_expected.to belong_to(:swimlane) }
+    it { is_expected.to belong_to(:before_swimlane) }
+    it { is_expected.to belong_to(:after_swimlane) }
+    it { is_expected.to have_one(:ticket) }
+    it { is_expected.to have_one(:board) }
+  end
+end
+
