3185f7d5ac5be3698830bffc94456428807a5288
John Cleary <john@createk.io>
2018-01-12 15:03:28 +0000

    `repo_id` exists on `tickets` so have to refer to that when joining

--- app/controllers/api/board_tickets_controller.rb
+++ app/controllers/api/board_tickets_controller.rb
@@ -5,6 +5,6 @@ class Api::BoardTicketsController < Api::BaseController
   def index
     @board_tickets = @board_tickets.includes(:board, :ticket, :swimlane)
     @board_tickets = @board_tickets.where(board_id: params[:board_id]) if params[:board_id].present?
-    @board_tickets = @board_tickets.where(repo_id: params[:repo_id]) if params[:repo_id].present?
+    @board_tickets = @board_tickets.where(tickets: { repo_id: params[:repo_id] }) if params[:repo_id].present?
   end
 end
