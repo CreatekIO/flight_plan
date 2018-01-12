33842e99bd8b680b61db6b0b910d072dfbb74606
John Cleary <john@createk.io>
2018-01-12 14:52:18 +0000

    add a crude key & secret authorisation mechanism

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,2 +1,12 @@
 class Api::BaseController < ActionController::Base
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    authenticate_or_request_with_http_token do |token, options|
+      key, secret = token.split(':')
+      key == ENV['USER_API_KEY'] && secret == ENV['USER_API_SECRET']
+    end
+  end
 end
