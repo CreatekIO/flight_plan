e67532247fbe6375654c4b108a2c55cfdf7d484b
Rob Paskin <rjpaskin+git@gmail.com>
2018-02-19 09:18:25 +0000

    Add importing of PR to `github:pull` Rake task
    For testing purposes we currently import all pull requests in the repo,
    but this could be narrowed to just open PRs, or PRs opened within N days
    ago.

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -1,3 +1,25 @@
 class PullRequest < ApplicationRecord
   belongs_to :repo
+
+  def self.import(remote_pr, remote_repo)
+    pull_request = find_by_remote(remote_pr, remote_repo)
+    pull_request.update_attributes(
+      remote_number: remote_pr[:number],
+      remote_title: remote_pr[:title],
+      remote_body: remote_pr[:body],
+      remote_state: remote_pr[:state],
+      remote_head_branch: remote_pr[:head][:ref],
+      remote_head_sha: remote_pr[:head][:sha],
+      remote_base_branch: remote_pr[:base][:ref],
+    )
+    pull_request
+  end
+
+  def self.find_by_remote(remote_pr, remote_repo)
+    pull_request = find_or_initialize_by(remote_id: remote_pr[:id])
+    if pull_request.repo_id.blank?
+      pull_request.repo = Repo.find_by!(remote_url: remote_repo[:full_name])
+    end
+    pull_request
+  end
 end
