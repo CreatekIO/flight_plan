a58c1faa6df6f3db3222cad99e0a5305f920c534
Rob Paskin <rjpaskin+git@gmail.com>
2018-02-23 16:54:12 +0000

    Add spec for PullRequestConnection model

--- spec/models/pull_request_connection_spec.rb
+++ spec/models/pull_request_connection_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PullRequestConnection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:pull_request) }
+    it { should belong_to(:ticket) }
+  end
 end
