e7cd8a1ec7e7a9388cccd64b7f5267a0590de1b0
John Cleary <john@createk.io>
2018-05-12 16:05:01 +0100

    load env variables from test file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'dotenv'
+Dotenv.overload('.env.test')
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
