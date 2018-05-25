e7367db0c395c81a9aa9a0b5fccf89a017205ab6
Rob Paskin <rjpaskin+git@gmail.com>
2018-05-25 15:26:28 +0100

    Don't print list of files when un-tar-ing Heroku CLI
    Means that you can't see any error messages in the CircleCI interface
    without downloading the command output as a file.

--- .circleci/setup_heroku.sh
+++ .circleci/setup_heroku.sh
@@ -4,7 +4,7 @@ set -ex
 apk --update add openssl # required for wget
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
