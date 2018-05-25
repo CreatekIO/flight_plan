f24ad106878eb85a70b2b371644344ad477e4e98
Rob Paskin <rjpaskin+git@gmail.com>
2018-05-25 15:37:05 +0100

    Don't need to install Heroku CLI on CircleCI
    Now that we use `release` in Procfile.

--- .circleci/setup_heroku.sh
+++ .circleci/setup_heroku.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 set -ex
 
-apk update
-apk add openssl # required for wget
-apk add openssh # required for ssh-keyscan
+apk --update add openssh # required for ssh-keyscan
 
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
-
-wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-
-mkdir -p /usr/local/lib /usr/local/bin
-tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
-
-cat > ~/.netrc << EOF
-machine api.heroku.com
-  login $HEROKU_LOGIN
-  password $HEROKU_API_KEY
-EOF
 
 cat >> ~/.ssh/config << EOF
 VerifyHostKeyDNS yes
