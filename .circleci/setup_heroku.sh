d7d341462ce7ac0895d4cda60b4c0ba650bb0619
Rob Paskin <rjpaskin+git@gmail.com>
2018-05-25 15:28:10 +0100

    Try to fix key authenticity prompt
    Deploys are timing out at the moment asking us to accept Heroku's SSH
    key on Git pushes.

--- .circleci/setup_heroku.sh
+++ .circleci/setup_heroku.sh
@@ -2,7 +2,12 @@
 set -ex
 
 apk --update add openssl # required for wget
+apk --update add openssh # required for ssh-keyscan
+
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
