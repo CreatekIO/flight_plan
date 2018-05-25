f9ada908af15764db7b19449778c3772424af21f
Rob Paskin <rjpaskin+git@gmail.com>
2018-05-25 15:02:52 +0100

    Add CommitStatus.import method

--- app/models/commit_status.rb
+++ app/models/commit_status.rb
@@ -1,3 +1,24 @@
 class CommitStatus < ApplicationRecord
   belongs_to :repo
+
+  def self.import(payload, repo)
+    create(
+      remote_id: payload[:id],
+      repo: repo,
+      state: payload[:state],
+      sha: payload[:sha],
+      description: payload[:description],
+      context: payload[:context],
+      url: payload[:target_url],
+      # May or may not appear - listed as only being available via API preview
+      avatar_url: payload[:avatar_url],
+      author_remote_id: payload.dig(:commit, :author, :id),
+      author_username: payload.dig(:commit, :author, :login),
+      committer_remote_id: payload.dig(:commit, :committer, :id),
+      committer_username: payload.dig(:commit, :committer, :login),
+      remote_created_at: payload[:created_at],
+      # For debugging purposes whilst developing
+      payload: payload
+    )
+  end
 end
--- spec/models/commit_status_spec.rb
+++ spec/models/commit_status_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe CommitStatus, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:repo) }
   end
+
+  describe '.import' do
+    let(:payload) { webhook_payload(:status) }
+    let(:repo) { create(:repo) }
+
+    subject(:repo_event) { described_class.import(payload, repo) }
+
+    it 'imports status details correctly' do
+      expect(repo_event.reload.attributes).to include(
+        'remote_id' => payload[:id],
+        'repo_id' => repo.id,
+        'state' => payload[:state],
+        'sha' => payload[:sha],
+        'description' => payload[:description],
+        'context' => payload[:context],
+        'url' => payload[:target_url],
+        'author_remote_id' => payload[:commit][:author][:id],
+        'author_username' => payload[:commit][:author][:login],
+        'committer_remote_id' => payload[:commit][:committer][:id],
+        'committer_username' => payload[:commit][:committer][:login],
+        'remote_created_at' => Time.zone.parse(payload[:created_at])
+      )
+    end
+  end
 end
