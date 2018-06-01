7672d740be6cb9525544838be8d353dfc47b7e50
John Cleary <john@createk.io>
2018-06-01 15:53:26 +0100

    force all responses to be JSON in the API

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,6 @@
 class Api::BaseController < ActionController::Base
   before_action :authenticate_user
+  before_action :set_default_response_format
 
   private
 
@@ -13,6 +14,10 @@ class Api::BaseController < ActionController::Base
   def request_http_token_authentication(realm = 'Application', message = nil)
     self.headers['WWW-Authenticate'] = %(Token realm="#{realm.gsub(/"/, '')}")
     render json: { error: 'HTTP Token: Access denied.' }, status: :unauthorized
+  end
+
+  def set_default_response_format
+    request.format = :json
   end
 
   def render_error(messages, status: :unprocessable_entity)
