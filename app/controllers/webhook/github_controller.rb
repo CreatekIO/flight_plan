f1eb7374c6d2adba7951d62608eebb4116b435fa
Rob Paskin <rjpaskin+git@gmail.com>
2018-07-05 12:38:37 +0100

    Correct wrong JSON key in comment import

--- app/controllers/webhook/github_controller.rb
+++ app/controllers/webhook/github_controller.rb
@@ -17,7 +17,7 @@ class Webhook::GithubController < Webhook::BaseController
       comment = Comment.find_by_remote(payload[:comment])
       comment.destroy if comment.persisted?
     else
-      Comment.import(payload[:comment], payload[:issue], payload[:repo])
+      Comment.import(payload[:comment], payload[:issue], payload[:repository])
     end
   end
 
