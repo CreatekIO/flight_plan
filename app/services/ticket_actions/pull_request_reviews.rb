66ac5764f1a1b27e1895f2f568b7004a44e51c0a
Rob Paskin <rjpaskin+git@gmail.com>
2018-08-03 16:21:24 +0100

    Add class for checking review status of a PR

---
+++ app/services/ticket_actions/pull_request_reviews.rb
@@ -0,0 +1,36 @@
+class TicketActions::PullRequestReviews < TicketActions::Base
+  def next_action
+    if reviews.none?
+      # everyone
+      positive 'Add a review', urls: "#{pull_request.html_url}/files"
+    elsif superceded_reviews.any?
+      # reviewers
+      warning 'Re-review updates', urls: pull_request.html_url
+    elsif pending_reviews.any?
+      # owner
+      warning 'Address changes', urls: pull_request.html_url
+    end
+  end
+
+  private
+
+  def reviews
+    @reviews ||= pull_request.reviews.reject do |review|
+      review.reviewer_remote_id == pull_request.creator_remote_id && review.approved?
+    end
+  end
+
+  def pending_reviews
+    @pending_reviews ||= reviews
+      .group_by(&:reviewer_remote_id)
+      .map {|_, user_reviews| user_reviews.max_by(&:remote_created_at) }
+      .select(&:changes_requested?)
+  end
+
+  # Assumes users always review latest commit
+  def superceded_reviews
+    @superceded_reviews ||= pending_reviews.select do |review|
+      pull_request.remote_head_sha != review.sha
+    end
+  end
+end
