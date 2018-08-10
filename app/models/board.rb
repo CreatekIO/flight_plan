c9df9cd82ecf7194104fedbc7787efbf2beb1632
Rob Paskin <rjpaskin+git@gmail.com>
2018-08-10 17:37:14 +0100

    Add dropdown to boards#show showing open pull requests
    Gives one place to look at all actions, rather than having to dig around
    the board.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,7 @@
 class Board < ApplicationRecord
   has_many :board_repos, dependent: :destroy
   has_many :repos, through: :board_repos
+  has_many :open_pull_requests, through: :repos
   has_many :swimlanes, dependent: :destroy
   has_many :board_tickets, dependent: :destroy
   has_many :releases, dependent: :destroy
