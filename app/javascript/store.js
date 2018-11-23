ac08a4ff5a8bf3db8346e5cd962801a03970a570
Rob Paskin <rjpaskin+git@gmail.com>
2018-11-23 15:00:10 +0000

    Add redux-thunk middleware

--- app/javascript/store.js
+++ app/javascript/store.js
@@ -1,10 +1,15 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
+
 import rootReducer from "./reducers";
 
-const { __REDUX_DEVTOOLS_EXTENSION__: devtools } = window;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = () => {
-    const store = createStore(rootReducer, devtools && devtools());
+    const store = createStore(
+        rootReducer,
+        composeEnhancers(applyMiddleware(thunkMiddleware))
+    );
 
     if (module.hot) {
         module.hot.accept("./reducers", () => store.replaceReducer(rootReducer));
