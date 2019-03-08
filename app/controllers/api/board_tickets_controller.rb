63d35d5b97fda79b35e5c6ae29c650295139c4f4
Reiss Johnson <reissjohnson@me.com>
2019-03-08 16:19:42 +0000

    basic way of creating GitHub tickets now in place

--- app/controllers/api/board_tickets_controller.rb
+++ app/controllers/api/board_tickets_controller.rb
@@ -1,4 +1,5 @@
 class Api::BoardTicketsController < Api::BaseController
+  before_action :find_board, only: :create
 
   load_and_authorize_resource
 
@@ -7,5 +8,22 @@ class Api::BoardTicketsController < Api::BaseController
     @board_tickets = @board_tickets.for_board(params[:board_id]) if params[:board_id].present?
     @board_tickets = @board_tickets.for_repo(params[:repo_id]) if params[:repo_id].present?
     @board_tickets = @board_tickets.where(tickets: { remote_number: params[:remote_number] }) if params[:remote_number].present?
+  end
+
+  def create
+    ticket = TicketCreationService.new(ticket_params).create_ticket
+    @board.board_tickets.create(ticket: ticket, swimlane: @board.swimlanes.first)
+  end
+
+  private
+
+  def find_board
+    @board = Board.find(params[:board_id])
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :repo_id)
   end
 end
