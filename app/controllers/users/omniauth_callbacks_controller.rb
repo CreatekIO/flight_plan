f042dee09d4e1462dbc4cd5e3d92d4ee8d9817cc
Rob Paskin <rjpaskin+git@gmail.com>
2019-04-26 13:02:11 +0100

    Refactor omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  before_action :ensure_org_member
+  before_action :ensure_org_member, only: :github
 
   def github
-    session['github.token'] = auth.credentials.token
-
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
+      user_session['github.token'] = auth.credentials.token
+
       set_flash_message(:notice, :success, kind: 'github') if is_navigational_format?
     else
-      session["devise.github_data"] = auth
+      session['devise.github_data'] = auth
       redirect_to new_user_registration_url
     end
+  end
+
+  def failure
+    redirect_to root_path, alert: params[:message].presence || 'Unable to sign in via GitHub'
   end
 
   private
 
   def ensure_org_member
     login = auth.extra.raw_info.login
-    unless Octokit.organization_member?('CreatekIO', login)
-      raise 'Not a member of Createk'
-    end
+
+    return if allowed_orgs.any? { |org| Octokit.organization_member?(org, login) }
+
+    redirect_to root_path, alert: 'Not a member of any permitted organisations' and return
   end
 
   def user
@@ -30,7 +35,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     request.env['omniauth.auth']
   end
 
-  def failure
-    redirect_to root_path
+  def allowed_orgs
+    ENV['PERMITTED_GITHUB_ORGS'].to_s.split(',').presence || %w[CreatekIO]
   end
 end
