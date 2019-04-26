9d84db44ff0eae6451b6dd35d19030954f6cf368
Rob Paskin <rjpaskin+git@gmail.com>
2019-04-26 14:08:55 +0100

    Identify ActionCable connection by `current_user`

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_user_from_session
+    end
+
+    private
+
+    def find_user_from_session
+      env['warden'].user.tap do |user|
+        reject_unauthorized_connection if user.blank?
+      end
+    end
   end
 end
