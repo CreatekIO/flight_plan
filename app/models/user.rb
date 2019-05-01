cb3f6ff4d21edc6c86bd16f7fb68ee16eb35ac8d
Rob Paskin <rjpaskin+git@gmail.com>
2019-05-01 12:11:20 +0100

    Capture GitHub username when user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   devise :registerable,
          :rememberable, :authenticatable,
-         :omniauthable, :omniauth_providers => [:github]
+         :omniauthable, omniauth_providers: [:github]
 
   has_many :pull_requests, foreign_key: :creator_remote_id, primary_key: :uid
   has_many :pull_request_reviews, foreign_key: :reviewer_remote_id, primary_key: :uid
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.name = auth.info.name   # assuming the user model has a name
+      user.name = auth.info.name
+      user.username = auto.info.nickname
     end
   end
 end
