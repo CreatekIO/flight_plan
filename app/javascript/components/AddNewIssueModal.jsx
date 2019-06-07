c01b53b9c145362f0341c840c422bdef56eedce4
Reiss Johnson <reissjohnson@me.com>
2019-06-07 16:56:53 +0100

    adds repo dropdown to new issue modal

--- app/javascript/components/AddNewIssueModal.jsx
+++ app/javascript/components/AddNewIssueModal.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Header, Image, Modal, Form } from 'semantic-ui-react';
+import { Button, Header, Image, Modal, Select, Form } from 'semantic-ui-react';
 import { ticketCreated } from '../action_creators';
 
 class AddNewIssueForm extends Component {
@@ -17,9 +17,26 @@ class AddNewIssueForm extends Component {
         ticketCreated(this.state);
     };
 
+    boardRepos = () => {
+        return flightPlanConfig.currentBoardRepos.map((repo, index, array) => ({
+            key: index,
+            text: repo.name,
+            value: repo.id
+        }));
+    };
+
     render() {
         return (
             <Form onSubmit={this.handleSubmit}>
+                <div className="field">
+                    <label>Repository</label>
+                    <Select
+                        placeholder={this.boardRepos()[0].text}
+                        name="repo_id"
+                        options={this.boardRepos()}
+                        onChange={this.handleChange}
+                    />
+                </div>
                 <Form.Input
                     label={'Title'}
                     placeholder={'Issue title'}
--- app/models/board_repo.rb
+++ app/models/board_repo.rb
@@ -1,4 +1,6 @@
 class BoardRepo < ApplicationRecord
   belongs_to :repo
   belongs_to :board
+
+  delegate :name, to: :repo
 end
