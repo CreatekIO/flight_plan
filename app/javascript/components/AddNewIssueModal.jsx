c7674bb5826bf524c6b37b56741b79baaf12d877
Reiss Johnson <reissjohnson@me.com>
2019-07-05 16:50:09 +0100

    modal now correctly closes after submitting board ticket

--- app/javascript/components/AddNewIssueModal.jsx
+++ app/javascript/components/AddNewIssueModal.jsx
@@ -3,18 +3,28 @@ import { connect } from "react-redux";
 import { Button, Header, Image, Modal, Select, Form } from "semantic-ui-react";
 import { ticketCreated } from "../action_creators";
 
-class AddNewIssueForm extends Component {
+class AddNewIssueModal extends Component {
     state = {
         title: "",
-        description: ""
+        description: "",
+        repo_id: ""
     };
 
     handleChange = (e, { name, value }) => {
         this.setState({ [name]: value });
     };
 
+    handleClose = () => {
+        this.setState({ showModal: false });
+    };
+
     handleSubmit = () => {
-        ticketCreated(this.state);
+        this.setState({ showModal: false });
+        ticketCreated({
+            title: this.state.title,
+            description: this.state.description,
+            repo_id: this.state.repo_id
+        });
     };
 
     boardRepos = () => {
@@ -27,41 +37,42 @@ class AddNewIssueForm extends Component {
 
     render() {
         return (
-            <Form onSubmit={this.handleSubmit}>
-                <div className="field">
-                    <label>Repository</label>
-                    <Select
-                        placeholder={this.boardRepos()[0].text}
-                        name="repo_id"
-                        options={this.boardRepos()}
-                        onChange={this.handleChange}
-                    />
-                </div>
-                <Form.Input
-                    label={"Title"}
-                    placeholder={"Issue title"}
-                    name="title"
-                    onChange={this.handleChange}
-                />
-                <Form.TextArea
-                    label={"Description"}
-                    placeholder={"Issue description"}
-                    name="description"
-                    onChange={this.handleChange}
-                />
-                <Form.Button>Submit</Form.Button>
-            </Form>
+            <Modal
+                onClose={this.handleClose}
+                open={this.state.showModal}
+                id={"add-new-issue-modal"}
+                trigger={<a className="item">Add an issue</a>}
+            >
+                <Modal.Header>Add a new issue</Modal.Header>
+                <Modal.Content>
+                    <Form onSubmit={this.handleSubmit}>
+                        <div className="field">
+                            <label>Repository</label>
+                            <Select
+                                placeholder={this.boardRepos()[0].text}
+                                name="repo_id"
+                                options={this.boardRepos()}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <Form.Input
+                            label={"Title"}
+                            placeholder={"Issue title"}
+                            name="title"
+                            onChange={this.handleChange}
+                        />
+                        <Form.TextArea
+                            label={"Description"}
+                            placeholder={"Issue description"}
+                            name="description"
+                            onChange={this.handleChange}
+                        />
+                        <Form.Button>Submit</Form.Button>
+                    </Form>
+                </Modal.Content>
+            </Modal>
         );
     }
 }
-
-const AddNewIssueModal = () => (
-    <Modal id={"add-new-issue-modal"} trigger={<a className="item">Add an issue</a>}>
-        <Modal.Header>Add a new issue</Modal.Header>
-        <Modal.Content>
-            <AddNewIssueForm />
-        </Modal.Content>
-    </Modal>
-);
 
 export default AddNewIssueModal;
