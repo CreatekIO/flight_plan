cd5edc9d5a514b3d176c01cc5557a3226b73aa59
Rob Paskin <rjpaskin+git@gmail.com>
2019-09-13 14:26:24 +0100

    Handle `TICKET_WAS_MOVED` action
    Remove the ticket (if we have it) from its current position (this may be
    different on different boards so we don't broadcast it) to the position
    specified in the payload - but only if we're 'windowed' onto the new
    position on the new swimlane, otherwise we just remove the ticket from
    view.

--- app/javascript/reducers/index.js
+++ app/javascript/reducers/index.js
@@ -80,6 +80,55 @@ const entitiesReducer = (state = initialEntitiesState, { type, payload }) => {
                 state
             );
         }
+        case "TICKET_WAS_MOVED": {
+            const { destinationId, destinationIndex } = payload;
+            let { boardTicket } = payload;
+
+            if (typeof boardTicket === "string") boardTicket = JSON.parse(boardTicket);
+
+            // Add board ticket to db
+            const newEntities = updateEntities(
+                normalize(boardTicket, boardTicketSchema).entities,
+                state
+            );
+
+            const transforms = [];
+
+            // Remove board ticket from wherever we have it currently (if at all)
+            Object.values(newEntities.swimlanes).forEach(swimlane =>
+                swimlane.board_tickets.forEach((boardTicketId, index) => {
+                    if (boardTicketId === boardTicket.id) {
+                        transforms.push({
+                            swimlanes: {
+                                [swimlane.id]: {
+                                    board_tickets: { $splice: [[index, 1]] }
+                                }
+                            }
+                        });
+                    }
+                })
+            );
+
+            const currentBoardTicketCount =
+                newEntities.swimlanes[destinationId].board_tickets.length;
+
+            if (currentBoardTicketCount >= destinationIndex) {
+                transforms.push({
+                    swimlanes: {
+                        [destinationId]: {
+                            board_tickets: {
+                                $splice: [[destinationIndex, 0, boardTicket.id]]
+                            }
+                        }
+                    }
+                });
+            }
+
+            return transforms.reduce(
+                (state, transform) => update(state, transform),
+                newEntities
+            );
+        }
         case "SWIMLANE_TICKETS_LOADING": {
             const { swimlaneId } = payload;
 
