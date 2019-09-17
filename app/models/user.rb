a6dfef6ecc6627af9c20e2faf31c5cfca6193127
Reiss Johnson <reissjohnson@me.com>
2019-09-17 10:57:46 +0100

    correct typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
-      user.username = auto.info.nickname
+      user.username = auth.info.nickname
     end
   end
 end
