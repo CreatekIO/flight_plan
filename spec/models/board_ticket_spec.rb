a256d263503a785ccaedd3cfddbda63acdcadb6d
Rob Paskin <rjpaskin+git@gmail.com>
2019-10-04 14:59:51 +0100

    Fix Rubocop violations in BoardTicket spec

--- spec/models/board_ticket_spec.rb
+++ spec/models/board_ticket_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe BoardTicket, type: :model do
 
     let(:remote_number) { '9' }
     let(:ticket) { create(:ticket, remote_number: remote_number, repo: repo) }
+
     subject { create(:board_ticket, board: board, ticket: ticket, swimlane: backlog) }
 
     before(:each) do
@@ -21,12 +22,15 @@ RSpec.describe BoardTicket, type: :model do
 
       stub_get_issue_labels_request
       stub_put_issue_labels_request
+
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('DO_NOT_SYNC_TO_GITHUB').and_return(nil)
     end
 
     context 'when an issue is moved to "Closed"' do
       it 'updates the GitHub issue via the API' do
-        stub = stub_request(:patch, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}").
-          with(body: "{\"state\":\"closed\"}")
+        stub = stub_request(:patch, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}")
+          .with(body: { state: 'closed' }.to_json)
 
         subject.swimlane = closed
         subject.save
@@ -39,28 +43,35 @@ RSpec.describe BoardTicket, type: :model do
       subject { create(:board_ticket, swimlane: closed, ticket: ticket, board: board) }
 
       it 'updates the GitHub issue via the API' do
-        stub = stub_request(:patch, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}").
-          with(body: "{\"state\":\"open\"}")
-          subject.swimlane = backlog
-          subject.save
+        stub = stub_request(:patch, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}")
+          .with(body: { state: 'open' }.to_json)
 
-          expect(stub).to have_been_requested
+        subject.swimlane = backlog
+        subject.save
+
+        expect(stub).to have_been_requested
       end
     end
 
     context 'with no state change' do
       it 'doesn\'t create/update any new timesheets' do
-        expect{ subject.touch }.to_not change{ Timesheet.count }
+        expect { subject.touch }.to_not change { Timesheet.count }
       end
     end
 
     context 'when state changes' do
       subject { create(:board_ticket, swimlane: backlog, ticket: ticket, board: board) }
+
       it 'creates a new open timesheet' do
         expect {
           subject.update_attributes(swimlane_id: dev.id)
         }.to change {
-          subject.timesheets.where(swimlane_id: dev.id, before_swimlane_id: backlog.id, ended_at: nil, after_swimlane_id: nil).count
+          subject.timesheets.where(
+            swimlane_id: dev.id,
+            before_swimlane_id: backlog.id,
+            ended_at: nil,
+            after_swimlane_id: nil
+          ).count
         }
       end
 
@@ -73,12 +84,13 @@ RSpec.describe BoardTicket, type: :model do
       end
 
       it 'changes labels on the remote' do
-        stub = stub_request(:put, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}/labels").
-          with(body: "[\"status: dev\"]")
-          subject.swimlane = dev
-          subject.save
+        stub = stub_request(:put, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}/labels")
+          .with(body: ['status: dev'].to_json)
 
-          expect(stub).to have_been_requested
+        subject.swimlane = dev
+        subject.save
+
+        expect(stub).to have_been_requested
       end
     end
   end
@@ -96,17 +108,17 @@ RSpec.describe BoardTicket, type: :model do
   end
 
   def stub_get_issue_labels_request
-    stub_request(:get, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}/labels?per_page=100").
-      to_return(
-        status: 200, 
-        body: [ ].to_json,
-        headers: {"Content-Type"=> "application/json"}
-    )
+    stub_request(:get, "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}/labels?per_page=100")
+      .to_return(
+        status: 200,
+        body: [].to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
   end
 
   def stub_put_issue_labels_request
     stub_request(
-      :put, 
+      :put,
       "https://api.github.com/repos/#{remote_url}/issues/#{remote_number}/labels"
     )
   end
