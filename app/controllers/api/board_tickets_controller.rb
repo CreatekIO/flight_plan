9e12f6de422295679483116bf0bf1535108e8970
Reiss Johnson <reissjohnson@me.com>
2019-10-15 11:55:06 +0100

    removes un-needed code

--- app/controllers/api/board_tickets_controller.rb
+++ app/controllers/api/board_tickets_controller.rb
@@ -1,4 +1,4 @@
-class Api::BoardTicketsController < AuthenticatedController
+class Api::BoardTicketsController < Api::BaseController
   load_and_authorize_resource
 
   def index
@@ -6,11 +6,5 @@ class Api::BoardTicketsController < AuthenticatedController
     @board_tickets = @board_tickets.for_board(params[:board_id]) if params[:board_id].present?
     @board_tickets = @board_tickets.for_repo(params[:repo_id]) if params[:repo_id].present?
     @board_tickets = @board_tickets.where(tickets: { remote_number: params[:remote_number] }) if params[:remote_number].present?
-  end
-
-  private
-
-  def ticket_params
-    params.require(:ticket).permit(:title, :description, :repo_id)
   end
 end
