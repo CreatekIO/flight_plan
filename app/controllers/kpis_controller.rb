f3d4fa591f1f99ef37a356ccc3e538ff749e177c
Rob Paskin <rjpaskin+git@gmail.com>
2020-04-03 14:24:56 +0100

    Add stats for bug tickets opened/closed to KPIs page
    Since we currently don't track when a ticket was closed, 'open' in this
    case means 'still open as of now'.

--- app/controllers/kpis_controller.rb
+++ app/controllers/kpis_controller.rb
@@ -1,6 +1,8 @@
 class KpisController < AuthenticatedController
   def index
     board = Board.find(params[:board_id])
+
+    @bug_tickets = BugTicketsCalculator.new(board)
     @cycle_time = CycleTimeCalculator.new(board)
     @circleci_builds = CircleciBuildsCalculator.new(board)
   end
