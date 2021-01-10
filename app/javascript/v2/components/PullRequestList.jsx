5985ebc5617bf86e7afabba73bb65a73d076b6bf
Rob Paskin <rjpaskin+git@gmail.com>
2021-01-10 10:59:35 +0000

    Add "v2" version of <PullRequestIcon/>
    Only used in one place - PullRequestList - so have inlined into that
    component.

--- app/javascript/v2/components/PullRequestList.jsx
+++ app/javascript/v2/components/PullRequestList.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Octicon, { GitMerge, GitPullRequest } from "@githubprimer/octicons-react";
 import classNames from "classnames";
 
 import NextActionButton from "./NextActionButton";
-import PullRequestIcon from "./PullRequestIcon";
 
 // FIXME: implement
 const Popup = ({ trigger }) => trigger;
+
+const pull_request_icons = {
+    open: { icon: GitPullRequest, className: "text-github-green" },
+    closed: { icon: GitPullRequest, className: "text-github-red" },
+    merged: { icon: GitMerge, className: "text-github-purple" }
+};
 
 const PullRequestLine = ({
     state,
@@ -14,31 +20,38 @@ const PullRequestLine = ({
     title,
     number,
     next_action
-}) => (
-    <div className="px-2 pt-1">
-        <div className="left floated content">
-            <PullRequestIcon merged={merged} state={state} />
-            &nbsp;
-            <Popup
-                trigger={
-                    <a href={html_url} target="_blank" className="text-blue-600 hover:text-blue-400 focus:text-blue-800">
-                        {number}
-                    </a>
-                }
-                content={title}
-                position="right center"
-                size="mini"
-                hideOnScroll
-                inverted
-            />
-        </div>
-        {next_action && (
-            <div className="right floated content">
-                <NextActionButton {...next_action} className="compact mini" />
+}) => {
+    const {
+        className: iconClassName,
+        icon
+    } = pull_request_icons[merged ? "merged" : state] || pull_request_icons.open;
+
+    return (
+        <div className="px-2 pt-1">
+            <div className="left floated content">
+                <Octicon icon={icon} className={classNames("mr-0.5", iconClassName)} />
+                &nbsp;
+                <Popup
+                    trigger={
+                        <a href={html_url} target="_blank" className="text-blue-600 hover:text-blue-400 focus:text-blue-800">
+                            {number}
+                        </a>
+                    }
+                    content={title}
+                    position="right center"
+                    size="mini"
+                    hideOnScroll
+                    inverted
+                />
             </div>
-        )}
-    </div>
-);
+            {next_action && (
+                <div className="right floated content">
+                    <NextActionButton {...next_action} className="compact mini" />
+                </div>
+            )}
+        </div>
+    );
+}
 
 export default function PullRequestList({ pullRequests, listStyle }) {
     return (
