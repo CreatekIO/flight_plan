a25686e0c4ca8919e0adfc506c95f941ffe0e2c4
Rob Paskin <rjpaskin+git@gmail.com>
2021-03-04 21:28:16 +0000

    Convert webhook spec from 'feature' type to request type
    Request specs use the whole stack, so are good enough facsimile to do an
    'end-to-end' webhook test.

--- app/services/github_webhook_fake.rb
+++ app/services/github_webhook_fake.rb
@@ -1,29 +1,27 @@
 class GithubWebhookFake
-  # See: https://developer.github.com/webhooks/#delivery-headers
-  class GitHubWebhookMiddleware < Faraday::Middleware
-    HMAC_DIGEST = OpenSSL::Digest.new('sha1')
-    USER_AGENT = 'GitHub-Hookshot/0000000'.freeze
+  HMAC_DIGEST = OpenSSL::Digest.new('sha1')
+  USER_AGENT = 'GitHub-Hookshot/0000000'.freeze
+  CONTENT_TYPE = 'application/x-www-form-urlencoded'.freeze
 
-    def call(env)
-      env.request_headers.merge!(
-        'X-GitHub-Delivery' => SecureRandom.uuid,
-        'X-Hub-Signature' => "sha1=#{generate_signature(env)}",
-        'User-Agent' => USER_AGENT,
-        'Content-Length' => env.body.bytesize.to_s
-      )
+  def self.generate_request(event:, payload:, secret:)
+    # Payload is JSON, even for URL-encoded deliveries
+    body = {
+      payload: payload.is_a?(String) ? payload : JSON.generate(payload)
+    }.to_query
 
-      @app.call(env)
-    end
+    signature = OpenSSL::HMAC.hexdigest(HMAC_DIGEST, secret, body)
 
-    private
+    # See: https://developer.github.com/webhooks/#delivery-headers
+    headers = {
+      'Content-Length' => body.bytesize.to_s,
+      'Content-Type' => CONTENT_TYPE,
+      'User-Agent' => USER_AGENT,
+      'X-GitHub-Delivery' => SecureRandom.uuid,
+      'X-GitHub-Event' => event.to_s,
+      'X-Hub-Signature' => "sha1=#{signature}"
+    }
 
-    def generate_signature(env)
-      OpenSSL::HMAC.hexdigest(
-        HMAC_DIGEST,
-        env.request.context.fetch(:webhook_secret),
-        env.body
-      )
-    end
+    [headers, body]
   end
 
   def initialize(url)
@@ -31,12 +29,16 @@ class GithubWebhookFake
   end
 
   def deliver(event:, payload:, secret:)
+    headers, body = self.class.generate_request(
+      event: event,
+      payload: payload,
+      secret: secret
+    )
+
     connection.post do |request|
       request.url(webhook_url)
-      # Payload is JSON, even for URL-encoded deliveries
-      request.body = { payload: payload.is_a?(String) ? payload : JSON.generate(payload) }
-      request.headers = { 'X-GitHub-Event' => event.to_s }
-      request.options.context = { webhook_secret: secret }
+      request.body = body
+      request.headers = headers
     end
   end
 
@@ -46,10 +48,7 @@ class GithubWebhookFake
 
   def connection
     @connection ||= Faraday.new do |conn|
-      conn.request :url_encoded
-      conn.use GitHubWebhookMiddleware
       conn.response :logger if ENV['ENABLE_WEBHOOK_FAKE_LOG'].present?
-
       conn.adapter Faraday.default_adapter
     end
   end
--- spec/support/webhook_helpers.rb
+++ spec/support/webhook_helpers.rb
@@ -1,4 +1,18 @@
 module WebhookHelpers
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def event_type(event, &block)
+      let(:event_type) { event }
+
+      context("`#{event}` event", &block)
+    end
+
+    def action(name, &block)
+      context("`#{name}` action", &block)
+    end
+  end
+
   def webhook_json(name)
     Rails.root.join("spec/support/fixtures/webhooks", "#{name}.json").read
   end
@@ -8,6 +22,16 @@ module WebhookHelpers
       webhook_json(name),
       object_class: HashWithIndifferentAccess
     )
+  end
+
+  def deliver_webhook(payload, event: event_type)
+    headers, request_body = GithubWebhookFake.generate_request(
+      event: event,
+      payload: payload,
+      secret: webhook_secret
+    )
+
+    post webhook_github_url, params: request_body, headers: headers
   end
 end
 
