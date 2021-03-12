9b2eb9e03404c9e6044c1726ce7dd0b7129e6da7
Rob Paskin <rjpaskin+git@gmail.com>
2021-03-12 15:27:28 +0000

    Shoulda Matchers now requires optional associations to be marked as such

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Timesheet do
   describe 'associations' do
     it { is_expected.to belong_to(:board_ticket) }
     it { is_expected.to belong_to(:swimlane) }
-    it { is_expected.to belong_to(:before_swimlane) }
-    it { is_expected.to belong_to(:after_swimlane) }
+    it { is_expected.to belong_to(:before_swimlane).optional }
+    it { is_expected.to belong_to(:after_swimlane).optional }
     it { is_expected.to have_one(:ticket) }
     it { is_expected.to have_one(:board) }
   end
