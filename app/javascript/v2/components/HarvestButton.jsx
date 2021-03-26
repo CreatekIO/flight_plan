2b7d70aa2041a70442d8fa454e4e6faeb688bf4c
Rob Paskin <rjpaskin+git@gmail.com>
2021-03-26 17:37:18 +0000

    Only load Harvest script when button clicked

--- app/javascript/v2/components/HarvestButton.jsx
+++ app/javascript/v2/components/HarvestButton.jsx
@@ -1,70 +1,85 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Octicon, { Clock } from "@githubprimer/octicons-react";
 import classNames from "classnames";
 
+import Loading from "./Loading";
 import { useEntity } from "../hooks";
 
-const notifyHarvestOfButton = element => {
+//===== Reference =====
+// - Harvest's docs:
+//   @ https://github.com/harvesthq/platform/blob/master/button.md
+// - Source of the Harvest Chrome extension:
+//   @ https://robwu.nl/crxviewer/?crx=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fharvest-time-tracker%2Ffbpiglieekigmkeebmeohkelfpjjlaia
+//   (specifically `js/profiles/github.js`)
+//=====================
+
+const notifyHarvestOfButton = event => new Promise((resolve, reject) => {
+    const { target: element, type } = event;
+
     const harvest = document.querySelector("#harvest-messaging");
-    if (!harvest) return;
+    if (!harvest) return reject();
 
-    const event = new CustomEvent("harvest-event:timers:add", {
-        detail: { element }
+    // Instruct Harvest to add its `click` listener
+    harvest.dispatchEvent(
+        new CustomEvent("harvest-event:timers:add", { detail: { element }})
+    );
+
+    // Re-dispatch click event so that Harvest's `click` listener is triggered
+    element.dispatchEvent(new MouseEvent(type, event));
+    resolve();
+});
+
+const loadHarvestScript = () => new Promise((resolve, reject) => {
+    if (document.querySelector("script[data-platform-config]")) return resolve();
+
+    const script = document.createElement("script");
+    script.src = "https://platform.harvestapp.com/assets/platform.js";
+    script.dataset.platformConfig = JSON.stringify({
+        applicationName: "GitHub",
+        skipStyling: true
     });
+    document.head.appendChild(script);
 
-    harvest.dispatchEvent(event);
-};
+    const onReady = () => {
+        document.body.removeEventListener("harvest-event:ready", onReady);
+        resolve();
+    };
+
+    document.body.addEventListener("harvest-event:ready", onReady);
+});
 
 const HarvestButton = ({ ticketId }) => {
-    const [harvestReady, setHarvestReady] = useState(false);
+    const [harvestState, setHarvestState] = useState("idle");
 
-    useEffect(() => {
-        if (document.querySelector("script[data-platform-config]")) {
-            setHarvestReady(true);
-            return;
-        }
+    const onClick = useCallback(({ nativeEvent }) => {
+        setHarvestState("loading");
 
-        const onReady = () => setHarvestReady(true);
-        document.body.addEventListener("harvest-event:ready", onReady);
-
-        const script = document.createElement("script");
-        script.src = "https://platform.harvestapp.com/assets/platform.js";
-        script.dataset.platformConfig = JSON.stringify({
-            applicationName: "GitHub",
-            skipStyling: true
-        });
-        document.head.appendChild(script);
-
-        return () => document.body.removeEventListener("harvest-event:ready", onReady);
-    }, []);
-
-    const ref = useRef(null);
+        loadHarvestScript()
+            .then(() => notifyHarvestOfButton(nativeEvent))
+            .then(() => setHarvestState("loaded"));
+    }, [setHarvestState]);
 
     const { number, title, repo: repoId } = useEntity("ticket", ticketId);
     const { slug } = useEntity("repo", repoId);
     const [_, repoName] = slug.split("/");
 
-    useEffect(() => {
-        if (!ref.current) return;
-        if (!harvestReady) return;
-
-        notifyHarvestOfButton(ref.current);
-    }, [ref.current, harvestReady]);
-
     return (
         <button
-            ref={ref}
-            disabled={!harvestReady}
+            onClick={onClick}
             type="button"
             className={classNames(
-                "block w-full text-xs text-gray-500 border border-gray-300 rounded px-4 py-1",
-                "hover:text-harvest-orange hover:border-harvest-orange"
+                "block w-full flex items-center justify-center text-xs text-gray-500 border border-gray-300 rounded px-4",
+                "hover:text-harvest-orange hover:border-harvest-orange",
+                harvestState === "loading" ? "py-1" : "py-1.5"
             )}
             data-item={JSON.stringify({ id: number, name: `#${number}: ${title}` })}
             data-group={JSON.stringify({ id: repoName, name: repoName })}
             data-permalink={`https://github.com/${slug}/issues/${number}`}
         >
-            <Octicon icon={Clock} className="mr-2" />
+            {harvestState === "loading"
+                ? <Loading size="xsmall" className="inline-block mr-2" />
+                : <Octicon icon={Clock} className="mr-2.5" />
+            }
             {" "}
             Track time with Harvest
         </button>
