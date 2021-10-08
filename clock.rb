d9fe6adc4364d958dad1f2c480ba5b410cf6fa95
Rob Paskin <rjpaskin+git@gmail.com>
2021-10-08 15:32:31 +0100

    Add logging to clockwork jobs and the workers they enqueue

--- clock.rb
+++ clock.rb
@@ -10,8 +10,12 @@ module Clockwork
   every 1.day, 'auto_deploy', at: '10:15', if: -> (time) { time.on_weekday? } do
     JobMonitor.measure('clock-auto_deploy') do
       Board.with_auto_deploy_repos.each do |board|
-        next if board.deploy_swimlane.tickets.none?
+        if board.deploy_swimlane.tickets.none?
+          Rails.logger.info("Nothing to deploy for Board##{id} '#{board.name}'")
+          next
+        end
 
+        Rails.logger.info("Enqueuing release for Board##{id} '#{board.name}'")
         DeployWorker.perform_async(board.id)
       end
     end
@@ -20,8 +24,12 @@ module Clockwork
   every 1.day, 'auto_merge', at: '10:30', if: -> (time) { time.on_weekday? } do
     JobMonitor.measure('clock-auto_merge') do
       Board.with_auto_deploy_repos.each do |board|
-        next if board.deploy_swimlane.tickets.none?
+        if board.deploy_swimlane.tickets.none?
+          Rails.logger.info("Nothing to merge for Board##{id} '#{board.name}'")
+          next
+        end
 
+        Rails.logger.info("Enqueuing merge check for Board##{id} '#{board.name}'")
         MergeWorker.perform_async(board.id)
       end
     end
