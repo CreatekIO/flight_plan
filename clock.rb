9ed929c906b8635c73dfa24af35d0904c137c9ae
Rob Paskin <rjpaskin+git@gmail.com>
2021-10-08 15:41:48 +0100

    Always run MergeWorker, regardless of tickets in deploy swimlane
    There may still be a release PR open, even if there are no (or we believe there
    aren't due to missed webhooks) tickets in the deploy swimlane.

--- clock.rb
+++ clock.rb
@@ -24,12 +24,8 @@ module Clockwork
   every 1.day, 'auto_merge', at: '10:30', if: -> (time) { time.on_weekday? } do
     JobMonitor.measure('clock-auto_merge') do
       Board.with_auto_deploy_repos.each do |board|
-        if board.deploy_swimlane.tickets.none?
-          Rails.logger.info("Nothing to merge for Board##{id} '#{board.name}'")
-          next
-        end
-
         Rails.logger.info("Enqueuing merge check for Board##{id} '#{board.name}'")
+
         MergeWorker.perform_async(board.id)
       end
     end
