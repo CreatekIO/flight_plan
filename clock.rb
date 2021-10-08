7d9f2a7cf00d011c4b3b95a4b902cb5b14ffea5e
Rob Paskin <rjpaskin+git@gmail.com>
2021-10-08 15:19:44 +0100

    Use JobMonitor in clockwork jobs

--- clock.rb
+++ clock.rb
@@ -8,18 +8,22 @@ module Clockwork
   end
 
   every 1.day, 'auto_deploy', at: '10:15', if: -> (time) { time.on_weekday? } do
-    Board.with_auto_deploy_repos.each do |board|
-      next if board.deploy_swimlane.tickets.none?
+    JobMonitor.measure('clock-auto_deploy') do
+      Board.with_auto_deploy_repos.each do |board|
+        next if board.deploy_swimlane.tickets.none?
 
-      DeployWorker.perform_async(board.id)
+        DeployWorker.perform_async(board.id)
+      end
     end
   end
 
   every 1.day, 'auto_merge', at: '10:30', if: -> (time) { time.on_weekday? } do
-    Board.with_auto_deploy_repos.each do |board|
-      next if board.deploy_swimlane.tickets.none?
+    JobMonitor.measure('clock-auto_merge') do
+      Board.with_auto_deploy_repos.each do |board|
+        next if board.deploy_swimlane.tickets.none?
 
-      MergeWorker.perform_async(board.id)
+        MergeWorker.perform_async(board.id)
+      end
     end
   end
 end
