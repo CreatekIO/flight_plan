111693771c7f21c3bbd3236b1cfee258776084ef
Rob Paskin <rjpaskin+git@gmail.com>
2021-10-22 12:59:02 +0100

    Add 'default' setting to Flipper features list
    To indicate which features are "on" by default, and thus should always
    be enabled in tests.

--- spec/support/flipper.rb
+++ spec/support/flipper.rb
@@ -1,23 +1,23 @@
 RSpec.configure do |config|
-  config.around(:each) do |example|
-    if example.metadata[:type] != :system
-      original = Flipper.instance
-      Flipper.instance = Flipper.new(Flipper::Adapters::Memory.new)
+  config.around(:each, type: -> (type) { type != :system }) do |example|
+    original = Flipper.instance
+    Flipper.instance = Flipper.new(Flipper::Adapters::Memory.new)
 
-      # Features currently checked server-side
-      %i[kpis self_serve_features].each do |feature|
-        Flipper.enable(feature)
-      end
+    example.run
 
-      example.run
+    Flipper.instance = original
+  end
 
-      Flipper.instance = original
-    else
-      example.run
+  config.before(:each) do
+    # Ensure "default" features are always enabled
+    ::FEATURES.select(&:default).map(&:name).each do |feature|
+      Flipper.enable(feature)
+    end
+  end
 
-      Flipper.features.each do |feature|
-        Flipper.adapter.clear(feature)
-      end
+  config.after(:each, type: :system) do
+    Flipper.features.each do |feature|
+      Flipper.adapter.clear(feature)
     end
   end
 end
