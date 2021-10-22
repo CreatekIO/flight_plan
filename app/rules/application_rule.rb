b0096652ffce5b6dda857fb95b7eb3d363a523be
Rob Paskin <rjpaskin+git@gmail.com>
2021-10-22 16:36:46 +0100

    Add base "rule" class
    Can be configured to listen for model changes, filtered by some criteria, then
    react to said changes.

---
+++ app/rules/application_rule.rb
@@ -0,0 +1,70 @@
+class ApplicationRule
+  include ModelListener
+
+  define_callbacks :execute, terminator: -> (_, callback) { !callback.call }
+
+  class_attribute :trigger_classes, instance_writer: false, default: [].freeze
+  attr_reader :event
+
+  def self.trigger(klass, event, *attrs, &block)
+    klass = klass.to_s
+    event = :updated if event == :changed
+    self.trigger_classes += [klass]
+
+    conditions = [
+      proc { record.class.name == klass && self.event == event }
+    ]
+
+    if event == :updated
+      attrs = attrs.to_set
+      conditions << proc { attrs.intersect?(changed_attributes) }
+    end
+
+    set_callback(:execute, if: conditions, &block)
+  end
+
+  private_class_method :trigger
+
+  def self.alias_record_as(name)
+    alias_method name, :record
+  end
+
+  private_class_method :alias_record_as
+
+  def self.listen!
+    Wisper.subscribe(self, scope: trigger_classes)
+  end
+
+  %i[created updated destroyed].each do |event|
+    class_eval <<~RUBY, __FILE__, __LINE__ + 1
+      def #{event}
+        @event = :#{event}
+        run_callbacks(:execute) { call }
+      end
+    RUBY
+  end
+
+  def call
+    raise NotImplementedError
+  end
+
+  private
+
+  def move(board_ticket, to:, position: :first)
+    return if board_ticket.blank?
+
+    swimlane = board_ticket.board.swimlanes.find_by(name: to)
+    return if swimlane.blank?
+
+    board_ticket.update_attributes!(
+      swimlane: swimlane,
+      swimlane_position: position
+    )
+  end
+
+  def halted_callback_hook(filter)
+    Rails.logger.info do
+      "#{self.class} for #{record.class}##{record.id} failed precondition #{filter.inspect}"
+    end
+  end
+end
