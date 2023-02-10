ac04c477dd7fda28f0f1dc8f42222ee39537a669
Rob Paskin <rjpaskin+git@gmail.com>
2023-02-10 16:36:00 +0000

    Remove Webpacker config files and binstubs

--- config/webpack/test.js
+++
@@ -1,7 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
-const webpackConfig = require('./base')
-
-delete webpackConfig.entry.component_demos
-
-module.exports = webpackConfig;
