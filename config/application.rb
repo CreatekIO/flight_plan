f700087d096ab6e778fee8a41b32925a4e968901
Rob Paskin <rjpaskin+git@gmail.com>
2023-05-07 22:48:50 +0100

    Update flipper-* gems
    Remove memoization middleware from config as it's now automatically
    added by the gem.

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,6 @@ module FlightPlan
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.middleware.use Flipper::Middleware::Memoizer
-
     config.time_zone = 'London'
 
     # Don't generate system test files.
