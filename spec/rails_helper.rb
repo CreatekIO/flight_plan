7fe65bd8477f3bc3c631adf4b0683ebd05785ec4
Rob Paskin <rjpaskin+git@gmail.com>
2023-05-07 23:31:21 +0100

    Update rspec-* gems
    Also update `rails_helper.rb` to match changes made to newly-generated
    file.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 ENV['RACK_ENV'] ||= 'test'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'spec_helper'
