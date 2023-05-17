0c39b2b4730e9747f781b3c74bd2d8731c517d27
James Thorp <jamesthorp1@gmail.com>
2023-05-17 12:11:25 +0100

    Add turb frame for creating new ideas and appending using turbo stream

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,4 +4,22 @@ class IdeasController < ApplicationController
   def index
     @ideas = Idea.all
   end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(ideas_params)
+    @idea.submitter = current_user
+    @created_idea = @idea
+    @idea = Idea.new if @idea.save
+
+    respond_to :turbo_stream
+
+  end
+
+  def ideas_params
+    params.require(:idea).permit(:title, :description)
+  end
 end
