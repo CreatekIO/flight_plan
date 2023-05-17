7325175d73147f849d6aae7a089a03b72fcc88aa
James Thorp <jamesthorp1@gmail.com>
2023-05-17 16:10:23 +0100

    add sortablejs package

--- app/packs/entrypoints/hotwire.js
+++ app/packs/entrypoints/hotwire.js
@@ -1,4 +1,16 @@
 import "@hotwired/turbo-rails";
-import { Application } from "@hotwired/stimulus";
+import { Application, Controller } from "@hotwired/stimulus";
+import Sortable from "sortablejs";
 
 const stimulus = Application.start();
+
+class SortableController extends Controller {
+
+  static targets = ['draggable']
+    connect(){
+      new Sortable(this.element, { draggable: '[data-sortable-target=draggable]' })
+    }
+
+ }
+
+ stimulus.register('sortable', SortableController)
