d5d43d2c54ea88c52ea2c690f18cd45af2245f1a
Rob Paskin <rjpaskin+git@gmail.com>
2023-05-18 16:09:05 +0100

    Remove logging, format JS file

--- app/packs/entrypoints/hotwire.js
+++ app/packs/entrypoints/hotwire.js
@@ -5,26 +5,21 @@ import Sortable from "sortablejs";
 const stimulus = Application.start();
 
 class SortableController extends Controller {
+    static targets = ['draggable', 'ideaIds'];
+    static values = { group: String };
 
-  static targets = ['draggable', 'ideaIds']
-  static values = { group: String }
     connect(){
       this.sortable = new Sortable(this.element, {
         draggable: '[data-sortable-target=draggable]',
         group: this.groupValue,
         animation: 200
-      })
+      });
     }
 
     sort(event) {
-        console.log('sort', event, this.sortable.toArray(), this.ideaIdsTarget);
-        this.ideaIdsTarget.value = this.sortable.toArray().join(',')
-        // this.ideaIdsTarget.form.requestSubmit()
-        // console.log(this.element, this.ideaIdsTarget.form)
+        this.ideaIdsTarget.value = this.sortable.toArray().join(',');
 
-        console.log(event.target, event.item, event.target.contains(event.item));
         if (!event.target.contains(event.item)) return;
-        console.log('should be one only');
 
         requestAnimationFrame(() => {
             this.ideaIdsTarget.form.requestSubmit();
@@ -33,4 +28,4 @@ class SortableController extends Controller {
 
  }
 
- stimulus.register('sortable', SortableController)
+ stimulus.register('sortable', SortableController);
