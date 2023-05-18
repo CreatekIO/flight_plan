48b4f0ad2476beec27d37c8edd5c1a334d84370a
Rob Paskin <rjpaskin+git@gmail.com>
2023-05-18 13:28:01 +0100

    Add status column to Idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,6 +3,8 @@ class Idea < ApplicationRecord
   before_create :set_default_position
   after_create_commit -> { broadcast_append_to "ideas" }
 
+  enum status: { pending: 'pending' }
+
   default_scope -> { order(:position) }
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_18_122022) do
+ActiveRecord::Schema.define(version: 2023_05_18_122502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 2023_05_18_122022) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
+    t.string "status", default: "pending"
     t.index ["submitter_id"], name: "index_ideas_on_submitter_id"
   end
 
