4281492c1e6d93ecb7503911a57aa9ef4ec99a9c
Rob Paskin <rjpaskin+git@gmail.com>
2023-05-18 13:23:22 +0100

    Add position column to Idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,13 @@
 class Idea < ApplicationRecord
   belongs_to :submitter, class_name: 'User'
+  before_create :set_default_position
   after_create_commit -> { broadcast_append_to "ideas" }
+
+  default_scope -> { order(:position) }
+
+  private
+
+  def set_default_position
+    self.position ||= self.class.maximum(:position)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_17_095131) do
+ActiveRecord::Schema.define(version: 2023_05_18_122022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(version: 2023_05_17_095131) do
     t.bigint "submitter_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "position"
     t.index ["submitter_id"], name: "index_ideas_on_submitter_id"
   end
 
